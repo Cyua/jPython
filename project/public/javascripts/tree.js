716a12ea4d77f0fb96622fecc0fad8d5e0552aeb
Cyua <466764008@qq.com>
2016-12-27 00:33:12 +0800

    .

---
+++ project/public/javascripts/tree.js
@@ -0,0 +1,14 @@
+class treeNode{
+	constructor(parentPtr, nName, nType, nValue, leftChild, rightChild){
+		this.parentPtr = parentPtr;
+		this.nName = nName;
+		this.nType = nType;
+		this.nValue = nValue;
+		this.leftChild = leftChild;
+		this.rightChild = rightChild;
+	}
+}
+
+function parserTree(lexResult){
+	return null;	
+}
