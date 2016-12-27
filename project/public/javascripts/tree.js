91c5ce1e443699d728ea8eb0508fc5473d6b0095
Cyua <466764008@qq.com>
2016-12-27 23:42:45 +0800

    .

--- project/public/javascripts/tree.js
+++ project/public/javascripts/tree.js
@@ -1,14 +1,24 @@
 class treeNode{
-	constructor(parentPtr, nName, nType, nValue, leftChild, rightChild){
+	constructor(parentPtr=null, nName=null, nType=null, nValue=null, leftChild=null, rightChild=null, next=null){
 		this.parentPtr = parentPtr;
 		this.nName = nName;
 		this.nType = nType;
 		this.nValue = nValue;
 		this.leftChild = leftChild;
 		this.rightChild = rightChild;
+		this.next = next;
 	}
 }
 
-function parserTree(lexResult){
+function parseTree(lexResult){
+	var root = new treeNode();
+	var prevNode = root;
+	var line = 0;
+	while(line < lexResult.length){
+		var lineType = null;
+		for(var index = 0; index < lexResult[line].length; index++){
+			var token = lexResult[line][index];	
+		}
+	}
 	return null;	
 }
